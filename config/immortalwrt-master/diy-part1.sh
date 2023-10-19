90ae946b62d79482fb04a140eadecc3cef03c3aa
Quansss <125027013+Quansss@users.noreply.github.com>
2023-10-19 11:34:28 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -10,5 +10,5 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
+rm -rf package/emortal/{autosamba,ipv6-helper}
 
