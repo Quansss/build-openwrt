580442ec82e839fe045eebb095b77e545328a6c5
Quansss <125027013+Quansss@users.noreply.github.com>
2023-10-26 10:50:39 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -24,8 +24,7 @@ sed -i 's/192.168.99.1/10.21.40.4/g' package/base-files/files/bin/config_generat
 #
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-svn co https://github.com/Zxilly/UA2F package/UA2F
+
 
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
